ea2d348784f3408009922f4e550a17ef45e29450
lingchao <75912001@qq.com>
2026-05-23 22:14:41 +0800

    1

--- gateway/header.strategy.go
+++ gateway/header.strategy.go
@@ -7,18 +7,18 @@ import (
 // DefaultHeaderStrategy 实现 IHeaderStrategy 接口，用于处理 xlib 24字节标准包头
 type DefaultHeaderStrategy struct{}
 
-func (s *DefaultHeaderStrategy) GetHeaderMode() xpacket.HeaderMode {
+func (p *DefaultHeaderStrategy) GetHeaderMode() xpacket.HeaderMode {
 	return xpacket.HeaderModeLengthFirst
 }
 
-func (s *DefaultHeaderStrategy) GetLengthSize() uint32 {
+func (p *DefaultHeaderStrategy) GetLengthSize() uint32 {
 	return 4
 }
 
-func (s *DefaultHeaderStrategy) UnpackLength(buf []byte) uint32 {
+func (p *DefaultHeaderStrategy) UnpackLength(buf []byte) uint32 {
 	return xpacket.GEndian.Uint32(buf[0:4])
 }
 
-func (s *DefaultHeaderStrategy) UnpackMessageID(buf []byte) uint32 {
+func (p *DefaultHeaderStrategy) UnpackMessageID(buf []byte) uint32 {
 	return xpacket.GEndian.Uint32(buf[4:8])
 }
