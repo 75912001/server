a2a9933af80008371e41e9ee662ec3fcad701888
lingchao <75912001@qq.com>
2026-05-24 23:17:59 +0800

    1

---
+++ common/ec.go
@@ -0,0 +1,10 @@
+package common
+
+import xerror "github.com/75912001/xlib/error"
+
+// [0x10000, 0x1FFFF]
+var (
+	ECGatewayOnlineNotFound = xerror.NewError(0x10000).
+		WithName("ECGatewayOnlineNotFound").
+		WithDesc("gateway-online-not-found-gateway未找到online服务")
+)
