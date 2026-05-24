7f1b4b6999dc1fcf740f9c1fd004150a2462f171
lingchao <75912001@qq.com>
2026-05-24 23:00:03 +0800

    1

--- gateway/user.handler.tcp.go
+++ gateway/user.handler.tcp.go
@@ -7,6 +7,7 @@ import (
 
 	pb "server/proto/pb"
 
+	xconfig "github.com/75912001/xlib/config"
 	xerror "github.com/75912001/xlib/error"
 	xlog "github.com/75912001/xlib/log"
 	xnetcommon "github.com/75912001/xlib/net/common"
@@ -23,14 +24,14 @@ type UserHandlerTCP struct{}
 
 // OnConnect 当客户端 TCP 建立成功：登记 User 并启动「未校验超时」定时器。
 func (h *UserHandlerTCP) OnConnect(remote xnetcommon.IRemote) error {
-	xlog.PrintInfo(fmt.Sprintf("Client connected from: %s", remote.GetIP()))
+	xlog.PrintInfo(fmt.Sprintf("Client connected from: %p %s", remote, remote.GetIP()))
 	GUserMgr.Add(remote)
 	return nil
 }
 
 // OnCheckPacketLength 检查包长度
 func (h *UserHandlerTCP) OnCheckPacketLength(length uint32) error {
-	if length < xpacket.HeaderSize || length > 65535 {
+	if length < xpacket.HeaderSize || length > *xconfig.GConfigMgr.Base.PacketLengthMax {
 		return xerror.Length
 	}
 	return nil
@@ -63,47 +64,63 @@ func (h *UserHandlerTCP) OnPacket(remote xnetcommon.IRemote, packet xpacket.IPac
 
 	header := pt.Header
 	body := pt.RawData
-	shardKey := fmt.Sprint(header.Key) // 以 uid 为分片键，保证同一用户路由到同一 online
 
 	xlog.PrintInfo(fmt.Sprintf("OnPacket MessageID: %d, Length: %d, Key: %d", header.MessageID, header.Length, header.Key))
 
-	// 消息 ID 1001：登录鉴权，走 Unary gRPC（selector.Sel 内部按 uid 路由，无需预选 online）
-	if header.MessageID == 1001 {
+	// UserVerifyReq：登录鉴权，走 Unary gRPC（selector.Sel 内部按 uid 路由，无需预选 online）
+	if header.MessageID == uint32(pb.MsgIDUser_UserVerifyReq_CMD) {
 		return unaryOnlineUserOnline(remote, header, body)
+	}
+
+	u := GUserMgr.Get(remote)
+	if u == nil {
+		xlog.PrintfErr("packet from unknown remote=%p messageID=%d", remote, header.MessageID)
+		return nil
 	}
 
 	// 心跳：网关本地处理（校验 session + 刷新心跳定时器），不下发到 online
 	if header.MessageID == uint32(pb.MsgIDUser_UserHeartbeatReq_CMD) {
-		u := GUserMgr.Get(remote)
-		if u == nil {
-			xlog.PrintfErr("heartbeat from unknown remote=%p", remote)
-			return nil
-		}
 		u.shard.PostHeartbeat(u, header, body)
 		return nil
 	}
 
-	// 其余消息：按 uid 哈希选取 online 实例，经双向 StreamTunnel 透传
-	online, err := GOnlineMgr.GetByShardKey(shardKey)
-	if err != nil {
-		xlog.PrintfErr("no online service for key=%s: %v", shardKey, err)
-		return err
+	if !u.IsVerified() {
+		xlog.PrintfErr("packet before verify, remote=%p messageID=%d", remote, header.MessageID)
+		u.Disconnect(xnetcommon.DisconnectReasonClientLogic)
+		return nil
 	}
 
-	frame := &pb.OnlineTunnelFrame{Uid: header.Key}
+	uid := u.uid.Load()
 
-	// 消息 ID 1003：断线信令
-	if header.MessageID == 1003 {
-		var offlineReq pb.OnlineUserOfflineReq
-		_ = proto.Unmarshal(body, &offlineReq)
-		frame.Payload = &pb.OnlineTunnelFrame_UserOfflineReq{UserOfflineReq: &offlineReq}
+	// 其余消息：使用登录时绑定的 online 实例，经双向 StreamTunnel 透传
+	online := u.GetOnline()
+	if online == nil {
+		xlog.PrintfErr("online not bound for uid=%d", uid)
+		u.Disconnect(xnetcommon.DisconnectReasonServerShutdown)
+		return nil
+	}
+
+	frame := &pb.OnlineTunnelFrame{Uid: uid}
+
+	// UserOfflineReq：客户端主动离线信令
+	if header.MessageID == uint32(pb.MsgIDUser_UserOfflineReq_CMD) {
+		var offlineReq pb.UserOfflineReq
+		if err := proto.Unmarshal(body, &offlineReq); err != nil {
+			xlog.PrintfErr("UserOfflineReq unmarshal failed uid=%d: %v", uid, err)
+			return err
+		}
+		frame.Payload = &pb.OnlineTunnelFrame_UserOfflineReq{
+			UserOfflineReq: &pb.OnlineUserOfflineReq{
+				Reason: offlineReq.GetReason(),
+			},
+		}
 	} else {
 		frame.Payload = &pb.OnlineTunnelFrame_ClientPacket{
 			ClientPacket: &pb.OnlineClientPacket{
 				MessageId: header.MessageID,
 				SessionId: header.SessionID,
 				ResultId:  header.ResultID,
-				Key:       header.Key,
+				Key:       uid,
 				Body:      body,
 			},
 		}
@@ -120,7 +137,11 @@ func (h *UserHandlerTCP) OnPacket(remote xnetcommon.IRemote, packet xpacket.IPac
 
 // OnDisconnect 当客户端连接断开：从 UserMgr 摘除并清理定时器。
 func (h *UserHandlerTCP) OnDisconnect(remote xnetcommon.IRemote) error {
-	xlog.PrintInfo(fmt.Sprintf("Client disconnected: %s", remote.GetIP()))
-	GUserMgr.Remove(remote)
+	u := GUserMgr.Remove(remote)
+	if u == nil {
+		xlog.PrintInfo(fmt.Sprintf("Client disconnected: %p reason=%d", remote, remote.GetDisconnectReason()))
+		return nil
+	}
+	xlog.PrintInfo(fmt.Sprintf("Client disconnected: %p %s reason=%d", remote, u.ip, remote.GetDisconnectReason()))
 	return nil
 }
