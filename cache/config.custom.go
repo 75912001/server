504f5081b7fdd872e70e5c56b07d940dd5f35201
lingchao <75912001@qq.com>
2026-05-27 01:04:34 +0800

    1

--- cache/config.custom.go
+++ cache/config.custom.go
@@ -6,16 +6,16 @@ import (
 	xconfig "github.com/75912001/xlib/config"
 )
 
-var GCfgCustomRedisKeyFormatUserData string
+var GCfgCustomRedisKeyFormatUserRecord string
 var GCfgCustomRedisKeyFormatUserToken string
 
 func initCustomConfig() {
-	GCfgCustomRedisKeyFormatUserData = xconfig.GConfigMgr.GetCustomString("redisKeyFormatUserData")
+	GCfgCustomRedisKeyFormatUserRecord = xconfig.GConfigMgr.GetCustomString("redisKeyFormatUserRecord")
 	GCfgCustomRedisKeyFormatUserToken = xconfig.GConfigMgr.GetCustomString("redisKeyFormatUserToken")
 }
 
 func RedisKeyUserData(uid uint64) string {
-	return fmt.Sprintf(GCfgCustomRedisKeyFormatUserData, uid)
+	return fmt.Sprintf(GCfgCustomRedisKeyFormatUserRecord, uid)
 }
 
 func RedisKeyUserToken(uid uint64, token string) string {
