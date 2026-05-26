5e8958737051748529b76b57576c6bba4404b1d8
lingchao <75912001@qq.com>
2026-05-27 00:13:57 +0800

    1

--- cache/redis.logic.go
+++ cache/redis.logic.go
@@ -7,14 +7,15 @@ import (
 
 // VerifyUserToken 验证用户令牌
 
-// RedisSetVerifyUserToken 设置用户令牌
-func (r *Redis) RedisSetVerifyUserToken(ctx context.Context, uid uint64, token string, expire time.Duration) (bool, error) {
-	return r.client.SetNX(ctx, cfgUserTokenKey(uid, token), "1", expire).Result()
+// SetVerifyUserToken 设置用户令牌
+func (r *Redis) SetVerifyUserToken(ctx context.Context, uid uint64, token string, expire time.Duration) (bool, error) {
+	key := RedisKeyUserToken(uid, token)
+	return r.client.SetNX(ctx, key, "1", expire).Result()
 }
 
-// RedisVerifyUserToken 验证用户令牌
-func (r *Redis) RedisVerifyUserToken(ctx context.Context, uid uint64, token string) (bool, error) {
-	key := cfgUserTokenKey(uid, token)
+// VerifyUserToken 验证用户令牌
+func (r *Redis) VerifyUserToken(ctx context.Context, uid uint64, token string) (bool, error) {
+	key := RedisKeyUserToken(uid, token)
 	res, err := r.client.Del(ctx, key).Result()
 	if err != nil {
 		return false, err
