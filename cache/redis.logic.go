6d07d57de4a0f0688148f9272d71281b524c05e2
lingchao <75912001@qq.com>
2026-05-27 02:13:32 +0800

    1

--- cache/redis.logic.go
+++ cache/redis.logic.go
@@ -2,23 +2,54 @@ package main
 
 import (
 	"context"
+	"server/proto/pb"
 	"time"
+
+	xruntime "github.com/75912001/xlib/runtime"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/proto"
 )
 
 // VerifyUserToken 验证用户令牌
 
 // SetVerifyUserToken 设置用户令牌
-func (r *Redis) SetVerifyUserToken(ctx context.Context, uid uint64, token string, expire time.Duration) (bool, error) {
+func (p *Redis) SetVerifyUserToken(ctx context.Context, uid uint64, token string, expire time.Duration) (bool, error) {
 	key := RedisKeyUserToken(uid, token)
-	return r.client.SetNX(ctx, key, "1", expire).Result()
+	return p.client.SetNX(ctx, key, "1", expire).Result()
 }
 
 // VerifyUserToken 验证用户令牌
-func (r *Redis) VerifyUserToken(ctx context.Context, uid uint64, token string) (bool, error) {
+func (p *Redis) VerifyUserToken(ctx context.Context, uid uint64, token string) (bool, error) {
 	key := RedisKeyUserToken(uid, token)
-	res, err := r.client.Del(ctx, key).Result()
+	res, err := p.client.Del(ctx, key).Result()
 	if err != nil {
-		return false, err
+		return false, errors.WithMessagef(err, "delete user token from redis failed, uid: %d, token: %s %v", uid, token, xruntime.Location())
 	}
 	return res == 1, nil
+}
+
+// UserRecord 用户记录
+
+func (p *Redis) SetUserRecord(ctx context.Context, uid uint64, record *pb.UserRecord) error {
+	data, err := proto.Marshal(record)
+	if err != nil {
+		return errors.WithMessagef(err, "marshal user record failed, uid: %d %v", uid, xruntime.Location())
+	}
+	key := RedisKeyUserRecord(uid)
+	if err := p.client.Set(ctx, key, data, 0).Err(); err != nil {
+		return errors.WithMessagef(err, "set user record to redis failed, uid: %d %v", uid, xruntime.Location())
+	}
+	return nil
+}
+
+func (p *Redis) GetUserRecord(ctx context.Context, uid uint64) (record *pb.UserRecord, err error) {
+	str, err := p.Get(ctx, RedisKeyUserRecord(uid))
+	if err != nil {
+		return nil, errors.WithMessagef(err, "get user record from redis failed, uid: %d %v", uid, xruntime.Location())
+	}
+	record = &pb.UserRecord{}
+	if err := proto.Unmarshal([]byte(str), record); err != nil {
+		return nil, errors.WithMessagef(err, "unmarshal user record failed, uid: %d %v", uid, xruntime.Location())
+	}
+	return record, nil
 }
