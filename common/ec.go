1e940da0ed7073b07f04abae76e65b4254197293
lingchao <75912001@qq.com>
2026-05-27 01:37:13 +0800

    1

--- common/ec.go
+++ common/ec.go
@@ -2,9 +2,19 @@ package common
 
 import xerror "github.com/75912001/xlib/error"
 
-// [0x10000, 0x1FFFF]
+// [0x10000,0x1FFFF] gateway
+// [0x20000,0x2FFFF] online
+// [0x30000,0x3FFFF] cache
 var (
+	// gateway
 	ECGatewayOnlineNotFound = xerror.NewError(0x10000).WithName("ECGatewayOnlineNotFound").WithDesc("gateway-online-not-found-gateway未找到online服务")
 	ECGatewayInvalidUID     = xerror.NewError(0x10001).WithName("ECGatewayInvalidUID").WithDesc("gateway-invalid-uid-uid无效")
 	ECGatewayUIDNotFound    = xerror.NewError(0x10002).WithName("ECGatewayUIDNotFound").WithDesc("gateway-uid-not-found-uid未找到")
+
+	// online
+
+	// cache
+	ECCacheInvalidArgument = xerror.NewError(0x30000).WithName("ECCacheInvalidArgument").WithDesc("cache-invalid-argument-参数无效")
+	ECCacheKeyNotFound     = xerror.NewError(0x30001).WithName("ECCacheKeyNotFound").WithDesc("cache-key-not-found-未找到key")
+	ECCacheRedisError      = xerror.NewError(0x30002).WithName("ECCacheRedisError").WithDesc("cache-redis-error-redis错误")
 )
