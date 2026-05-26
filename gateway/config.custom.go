78ab0aac63267a3d8ccdb29f4051aec19b5b607f
lingchao <75912001@qq.com>
2026-05-27 01:04:07 +0800

    1

--- gateway/config.custom.go
+++ gateway/config.custom.go
@@ -13,7 +13,3 @@ func initCustomConfig() {
 	GCfgCustomVerifyExpireTimeDuration = xconfig.GConfigMgr.GetCustomDuration("verifyExpireTimeDuration", 300*time.Second)
 	GCfgCustomHeartBeatExpireDuration = xconfig.GConfigMgr.GetCustomDuration("heartBeatExpireTimeDuration", 300*time.Second)
 }
-
-func cfgHeartBeatExpireSecond() int64 {
-	return int64(GCfgCustomHeartBeatExpireDuration.Seconds())
-}
