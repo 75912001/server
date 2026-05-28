ccee8d0fd638a8915d416a14bac31ccbe9ee1d3c
lingchao <75912001@qq.com>
2026-05-28 13:47:44 +0800

    1

--- common/common.go
+++ common/common.go
@@ -2,6 +2,9 @@ package common
 
 import (
 	xnetcommon "github.com/75912001/xlib/net/common"
+	xpacket "github.com/75912001/xlib/packet"
+	xruntime "github.com/75912001/xlib/runtime"
+	xruntimeconstants "github.com/75912001/xlib/runtime/constants"
 )
 
 const (
@@ -22,3 +25,8 @@ const (
 	// [10000,20000] 留给业务使用
 	DisconnectReason_xxx xnetcommon.DisconnectReason = 10000 // 未知原因
 )
+
+func init() {
+	xruntime.SetRunMode(xruntimeconstants.RunModeDebug)
+	xpacket.SetEndianMode(xpacket.LittleEndian)
+}
