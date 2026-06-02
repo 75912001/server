1fca66076249520aaf6e2b9a055b126a6c0b6a0c
lingchao <75912001@qq.com>
2026-06-02 12:02:26 +0800

    1

--- online/user.actor.go
+++ online/user.actor.go
@@ -2,26 +2,19 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	pb "server/proto/pb"
 
 	xactor "github.com/75912001/xlib/actor"
 	xlog "github.com/75912001/xlib/log"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 const (
-	OnlineUserActorCmdKickOldGateway xactor.CMD = 100
-	OnlineUserActorCmdLogin          xactor.CMD = 101
-	OnlineUserActorCmdOffline        xactor.CMD = 102
+	OnlineUserActorCmdLogin   xactor.CMD = 101
+	OnlineUserActorCmdOffline xactor.CMD = 102
 )
 
 func (p *User) PostLogin(req *pb.OnlineUserOnlineReq) (*pb.OnlineUserOnlineRes, error) {
-	if _, err := p.actor.SendMsgSync(xactor.NewMsg(context.Background(), OnlineUserActorCmdKickOldGateway)); err != nil {
-		return nil, err
-	}
 	resp, err := p.actor.SendMsgSync(xactor.NewMsg(context.Background(), OnlineUserActorCmdLogin, req))
 	if err != nil {
 		return nil, err
@@ -47,13 +40,6 @@ func (p *User) behavior(messages ...any) (xactor.Behavior, any, error) {
 			continue
 		}
 		switch msg.Cmd {
-		case OnlineUserActorCmdKickOldGateway:
-			if p.gatewayID != "" {
-				if err := p.kickOldGateway(); err != nil {
-					xlog.PrintfErr("kick old gateway failed uid=%d gateway=%s err=%v", p.uid, p.gatewayID, err)
-					return p.behavior, resp, status.Error(codes.FailedPrecondition, fmt.Sprintf("kick old gateway failed: %v", err))
-				}
-			}
 		case OnlineUserActorCmdLogin:
 			req, ok := msg.Args[0].(*pb.OnlineUserOnlineReq)
 			if !ok {
