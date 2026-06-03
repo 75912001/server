249f145dc524de5136330f2d21040932b6bf66ec
lingchao <75912001@qq.com>
2026-06-04 00:54:08 +0800

    1

--- common/common.go
+++ common/common.go
@@ -1,6 +1,11 @@
 package common
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"strconv"
+
 	xnetcommon "github.com/75912001/xlib/net/common"
 	xpacket "github.com/75912001/xlib/packet"
 	xruntime "github.com/75912001/xlib/runtime"
@@ -19,6 +24,10 @@ const (
 	CacheServerName  = "cache"
 	CachePackageName = "cache"
 	CacheServiceName = "CacheService"
+
+	LoginServerName = "login"
+
+	GatewaySessionSecret = "menglc-session"
 )
 
 const (
@@ -29,4 +38,26 @@ const (
 func init() {
 	xruntime.SetRunMode(xruntimeconstants.RunModeDebug)
 	xpacket.SetEndianMode(xpacket.LittleEndian)
+}
+
+func NewGatewaySession(uid uint64, gatewayKey string, gatewayNonce string) string {
+	data := strconv.FormatUint(uid, 10) + ":" + gatewayKey + ":" + gatewayNonce + ":" + GatewaySessionSecret
+	sum := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(sum[:])
+}
+
+func NewGatewayNonce() (string, error) {
+	return newRandomHex32()
+}
+
+func NewRandomGatewaySession() (string, error) {
+	return newRandomHex32()
+}
+
+func newRandomHex32() (string, error) {
+	var data [32]byte
+	if _, err := rand.Read(data[:]); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(data[:]), nil
 }
