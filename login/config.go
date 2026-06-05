e0e2131815ebc6302af9738a957112ed13300faa
lingchao <75912001@qq.com>
2026-06-05 17:46:11 +0800

    1

--- login/config.go
+++ login/config.go
@@ -3,6 +3,8 @@ package main
 import (
 	"time"
 
+	"server/common"
+
 	xconfig "github.com/75912001/xlib/config"
 )
 
@@ -10,11 +12,11 @@ var GCfgCustomHTTPAddr string
 var GCfgCustomTokenPath string
 var GCfgCustomSessionPath string
 var GCfgCustomTokenExpireSecond uint64
-var GCfgCustomSessionExpireSecond uint64
+var GCfgCustomTicketExpireSecond uint64
+var GCfgCustomTicketSecret string
 var GCfgCustomReadHeaderTimeout time.Duration
 var GCfgCustomShutdownTimeout time.Duration
 var GCfgCustomCacheRPCTimeout time.Duration
-var GCfgCustomGatewayRPCTimeout time.Duration
 var GCfgCustomMaxBodyBytes int64
 
 func initCustomConfig() {
@@ -22,10 +24,10 @@ func initCustomConfig() {
 	GCfgCustomTokenPath = xconfig.GConfigMgr.GetCustomString("tokenPath", "/api/login/token")
 	GCfgCustomSessionPath = xconfig.GConfigMgr.GetCustomString("sessionPath", "/api/login/session")
 	GCfgCustomTokenExpireSecond = uint64(xconfig.GConfigMgr.GetCustomDuration("tokenExpireSecond", 10*time.Second) / time.Second)
-	GCfgCustomSessionExpireSecond = uint64(xconfig.GConfigMgr.GetCustomDuration("sessionExpireSecond", 30*time.Second) / time.Second)
+	GCfgCustomTicketExpireSecond = uint64(xconfig.GConfigMgr.GetCustomDuration("ticketExpireSecond", 10*time.Second) / time.Second)
+	GCfgCustomTicketSecret = xconfig.GConfigMgr.GetCustomString("ticketSecret", common.ConnectTicketSecretDefault)
 	GCfgCustomReadHeaderTimeout = xconfig.GConfigMgr.GetCustomDuration("readHeaderTimeout", 5*time.Second)
 	GCfgCustomShutdownTimeout = xconfig.GConfigMgr.GetCustomDuration("shutdownTimeout", 10*time.Second)
 	GCfgCustomCacheRPCTimeout = xconfig.GConfigMgr.GetCustomDuration("cacheRPCTimeout", 3*time.Second)
-	GCfgCustomGatewayRPCTimeout = xconfig.GConfigMgr.GetCustomDuration("gatewayRPCTimeout", 3*time.Second)
 	GCfgCustomMaxBodyBytes = xconfig.GConfigMgr.GetCustomInt64("maxBodyBytes", 4096)
 }
