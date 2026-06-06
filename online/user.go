6d5d7d50164790d31998d87f77d1f2065ac61aba
lingchao <75912001@qq.com>
2026-06-07 01:49:23 +0800

    1

--- online/user.go
+++ online/user.go
@@ -8,24 +8,22 @@ import (
 )
 
 type User struct {
-	uid        uint64
-	account    string
-	gatewayID  string
-	clientIP   string
-	sessionMgr *userSessionMgr
-	userRecord *pb.UserRecord
-	actor      *xactor.Actor[uint64]
+	uid         uint64
+	account     string
+	gatewayID   string
+	userSession string
+	clientIP    string
+	userRecord  *pb.UserRecord
+	actor       *xactor.Actor[uint64]
 }
 
 func newUser(uid uint64) *User {
 	u := &User{uid: uid}
-	u.sessionMgr = newUserSessionMgr(u)
 	u.actor = xactor.NewActor[uint64](uid, nil, u.behavior)
 	u.actor.Start()
 	return u
 }
 
 func (p *User) Stop() {
-	p.sessionMgr.Stop()
 	p.actor.SendMsg(xactor.NewMsg(context.Background(), xactor.SystemReservedCommand_Stop))
 }
