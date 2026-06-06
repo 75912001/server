3940fb4d15ef8f3e85435cf2a53d91b981a53a10
lingchao <75912001@qq.com>
2026-06-06 19:42:25 +0800

    1

--- gateway/config.custom.go
+++ gateway/config.custom.go
@@ -3,13 +3,17 @@ package main
 import (
 	"time"
 
+	"server/common"
+
 	xconfig "github.com/75912001/xlib/config"
 )
 
 var GCfgCustomVerifyExpireTimeDuration time.Duration
 var GCfgCustomHeartBeatExpireDuration time.Duration
+var GCfgCustomTicketSecret string
 
 func initCustomConfig() {
 	GCfgCustomVerifyExpireTimeDuration = xconfig.GConfigMgr.GetCustomDuration("verifyExpireTimeDuration", 300*time.Second)
 	GCfgCustomHeartBeatExpireDuration = xconfig.GConfigMgr.GetCustomDuration("heartBeatExpireTimeDuration", 300*time.Second)
+	GCfgCustomTicketSecret = xconfig.GConfigMgr.GetCustomString("ticketSecret", common.ConnectTicketSecretDefault)
 }
