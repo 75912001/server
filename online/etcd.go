19982a4ce1d33782cbbcf53ed31fde6fab85ebdf
lingchao <75912001@qq.com>
2026-06-06 23:31:27 +0800

    1

--- online/etcd.go
+++ online/etcd.go
@@ -20,12 +20,6 @@ func onEtcdAdd(args ...any) error {
 		return nil
 	}
 	switch serverName {
-	case common.GatewayServerName:
-		if err := GGatewayMgr.AddByEtcd(key, valueJson); err != nil {
-			xlog.GLog.Fatalf("onEtcdAdd key=%s: %v", key, err)
-			return err
-		}
-		xlog.GLog.Infof("onEtcdAdd key:%s", key)
 	case common.CacheServerName:
 		GCacheMgr.Remove(key)
 		if err := GCacheMgr.Add(key, valueJson); err != nil {
@@ -47,9 +41,6 @@ func onEtcdDel(args ...any) error {
 	key := args[0].(string)
 	_, _, serverName, _ := xetcd.Parse(key)
 	switch serverName {
-	case common.GatewayServerName:
-		GGatewayMgr.Remove(key)
-		xlog.GLog.Infof("onEtcdDel key:%s", key)
 	case common.CacheServerName:
 		GCacheMgr.Remove(key)
 		xlog.GLog.Infof("onEtcdDel key:%s", key)
