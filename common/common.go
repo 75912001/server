e1080e548ae4d3e36dad86592b7cabd888e40b72
lingchao <75912001@qq.com>
2026-06-08 13:57:09 +0800

    refactor: simplify common.go by removing unused functions and constants

--- common/common.go
+++ common/common.go
@@ -1,15 +1,8 @@
 package common
 
 import (
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/hex"
-	"strconv"
-
 	xnetcommon "github.com/75912001/xlib/net/common"
 	xpacket "github.com/75912001/xlib/packet"
-	xruntime "github.com/75912001/xlib/runtime"
-	xruntimeconstants "github.com/75912001/xlib/runtime/constants"
 )
 
 const (
@@ -27,7 +20,7 @@ const (
 
 	LoginServerName = "login"
 
-	GatewaySessionSecret = "menglc-session"
+	ConnectTicketSecretDefault = "server-dev-connect-ticket-secret"
 )
 
 const (
@@ -36,28 +29,5 @@ const (
 )
 
 func init() {
-	xruntime.SetRunMode(xruntimeconstants.RunModeDebug)
 	xpacket.SetEndianMode(xpacket.LittleEndian)
-}
-
-func NewGatewaySession(uid uint64, gatewayKey string, gatewayNonce string) string {
-	data := strconv.FormatUint(uid, 10) + ":" + gatewayKey + ":" + gatewayNonce + ":" + GatewaySessionSecret
-	sum := sha256.Sum256([]byte(data))
-	return hex.EncodeToString(sum[:])
-}
-
-func NewGatewayNonce() (string, error) {
-	return newRandomHex32()
-}
-
-func NewRandomGatewaySession() (string, error) {
-	return newRandomHex32()
-}
-
-func newRandomHex32() (string, error) {
-	var data [32]byte
-	if _, err := rand.Read(data[:]); err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(data[:]), nil
 }
