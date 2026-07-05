059e69cba602cfb3c1eb471f8677c9eed8692c70
lingchao <75912001@qq.com>
2026-07-05 20:57:35 +0800

    feat: add deploy script for automated Docker service deployment

--- common/gameconfig/constants.go
+++ common/gameconfig/constants.go
@@ -4,43 +4,18 @@ import pb "server/proto/pb"
 
 const petSavedBaseGradeOffsetMin = -2
 
-var elementOrder = []pb.AssetElemental{
-	pb.AssetElemental_AssetElemental_Earth,
-	pb.AssetElemental_AssetElemental_Water,
-	pb.AssetElemental_AssetElemental_Fire,
-	pb.AssetElemental_AssetElemental_Wind,
+func isPetID(id uint32) bool {
+	return id >= uint32(pb.AssetIDRange_AssetIDRange_Pet_Start) && id <= uint32(pb.AssetIDRange_AssetIDRange_Pet_End)
 }
 
-var elementByKey = map[string]pb.AssetElemental{
-	"earth": pb.AssetElemental_AssetElemental_Earth,
-	"water": pb.AssetElemental_AssetElemental_Water,
-	"fire":  pb.AssetElemental_AssetElemental_Fire,
-	"wind":  pb.AssetElemental_AssetElemental_Wind,
+func isPetSkillID(id uint32) bool {
+	return id >= uint32(pb.AssetIDRange_AssetIDRange_Pet_Skill_Start) && id <= uint32(pb.AssetIDRange_AssetIDRange_Pet_Skill_End)
 }
 
-var petAttributeKeys = []string{
-	"poisonResist",
-	"paralysisResist",
-	"sleepResist",
-	"stoneResist",
-	"drunkResist",
-	"confusionResist",
-	"critical",
-	"counter",
+func isCharacterID(id uint32) bool {
+	return id >= uint32(pb.AssetIDRange_AssetIDRange_Character_Start) && id <= uint32(pb.AssetIDRange_AssetIDRange_Character_End)
 }
 
-func isPetID(id int) bool {
-	return id >= int(pb.AssetIDRange_AssetIDRange_Pet_Start) && id <= int(pb.AssetIDRange_AssetIDRange_Pet_End)
-}
-
-func isPetSkillID(id int) bool {
-	return id >= int(pb.AssetIDRange_AssetIDRange_Pet_Skill_Start) && id <= int(pb.AssetIDRange_AssetIDRange_Pet_Skill_End)
-}
-
-func isCharacterID(id int) bool {
-	return id >= int(pb.AssetIDRange_AssetIDRange_Character_Start) && id <= int(pb.AssetIDRange_AssetIDRange_Character_End)
-}
-
-func isSceneID(id int) bool {
-	return id >= int(pb.AssetIDRange_AssetIDRange_Scene_Start) && id <= int(pb.AssetIDRange_AssetIDRange_Scene_End)
+func isSceneID(id uint32) bool {
+	return id >= uint32(pb.AssetIDRange_AssetIDRange_Scene_Start) && id <= uint32(pb.AssetIDRange_AssetIDRange_Scene_End)
 }
