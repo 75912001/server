717f05360168dcdd8e9c4bb4a5da9ca613590447
lingchao <75912001@qq.com>
2026-07-18 19:31:59 +0800

    feat: add character skill configuration to types.go

--- common/gameconfig/manager.go
+++ common/gameconfig/manager.go
@@ -1,81 +1,90 @@
 package gameconfig
 
-func Load(dir string) (*Manager, error) {
-	manager := &Manager{
-		PetSkill:  newPetSkillConfig(),
-		Pet:       newPetConfig(),
-		Character: newCharacterConfig(),
-		Enemy:     newEnemyGroupConfig(),
-		EnemyExp:  newEnemyExpConfig(),
-		Scene:     newSceneConfig(),
-		Exp:       newExpConfig(),
+func Load(dir string) error {
+	GGameConfig = &Manager{
+		CharacterSkill: newCharacterSkillConfig(),
+		PetSkill:       newPetSkillConfig(),
+		Pet:            newPetConfig(),
+		Character:      newCharacterConfig(),
+		Enemy:          newEnemyGroupConfig(),
+		EnemyExp:       newEnemyExpConfig(),
+		Scene:          newSceneConfig(),
+		Exp:            newExpConfig(),
+	}
+	if err := GGameConfig.CharacterSkill.load(dir); err != nil {
+		return err
+	}
+	if err := GGameConfig.PetSkill.load(dir); err != nil {
+		return err
+	}
+	if err := GGameConfig.Pet.load(dir); err != nil {
+		return err
+	}
+	if err := GGameConfig.Character.load(dir); err != nil {
+		return err
+	}
+	if err := GGameConfig.Enemy.load(dir); err != nil {
+		return err
+	}
+	if err := GGameConfig.EnemyExp.load(dir); err != nil {
+		return err
+	}
+	if err := GGameConfig.Scene.load(dir); err != nil {
+		return err
+	}
+	if err := GGameConfig.Exp.load(dir); err != nil {
+		return err
 	}
 
-	if err := manager.PetSkill.load(dir); err != nil {
-		return nil, err
+	if err := GGameConfig.CharacterSkill.check(); err != nil {
+		return err
 	}
-	if err := manager.Pet.load(dir); err != nil {
-		return nil, err
+	if err := GGameConfig.PetSkill.check(); err != nil {
+		return err
 	}
-	if err := manager.Character.load(dir); err != nil {
-		return nil, err
+	if err := GGameConfig.Pet.check(); err != nil {
+		return err
 	}
-	if err := manager.Enemy.load(dir); err != nil {
-		return nil, err
+	if err := GGameConfig.Character.check(); err != nil {
+		return err
 	}
-	if err := manager.EnemyExp.load(dir); err != nil {
-		return nil, err
+	if err := GGameConfig.Enemy.check(); err != nil {
+		return err
 	}
-	if err := manager.Scene.load(dir); err != nil {
-		return nil, err
+	if err := GGameConfig.EnemyExp.check(); err != nil {
+		return err
 	}
-	if err := manager.Exp.load(dir); err != nil {
-		return nil, err
+	if err := GGameConfig.Scene.check(); err != nil {
+		return err
 	}
-
-	if err := manager.PetSkill.check(); err != nil {
-		return nil, err
-	}
-	if err := manager.Pet.check(); err != nil {
-		return nil, err
-	}
-	if err := manager.Character.check(); err != nil {
-		return nil, err
-	}
-	if err := manager.Enemy.check(); err != nil {
-		return nil, err
-	}
-	if err := manager.EnemyExp.check(); err != nil {
-		return nil, err
-	}
-	if err := manager.Scene.check(); err != nil {
-		return nil, err
-	}
-	if err := manager.Exp.check(); err != nil {
-		return nil, err
+	if err := GGameConfig.Exp.check(); err != nil {
+		return err
 	}
 
-	if err := manager.PetSkill.assemble(); err != nil {
-		return nil, err
+	if err := GGameConfig.CharacterSkill.assemble(); err != nil {
+		return err
 	}
-	if err := manager.Pet.assemble(); err != nil {
-		return nil, err
+	if err := GGameConfig.PetSkill.assemble(); err != nil {
+		return err
 	}
-	if err := manager.Character.assemble(); err != nil {
-		return nil, err
+	if err := GGameConfig.Pet.assemble(); err != nil {
+		return err
 	}
-	if err := manager.Enemy.assemble(); err != nil {
-		return nil, err
+	if err := GGameConfig.Character.assemble(); err != nil {
+		return err
 	}
-	if err := manager.EnemyExp.assemble(); err != nil {
-		return nil, err
+	if err := GGameConfig.Enemy.assemble(); err != nil {
+		return err
 	}
-	if err := manager.Scene.assemble(); err != nil {
-		return nil, err
+	if err := GGameConfig.EnemyExp.assemble(); err != nil {
+		return err
 	}
-	if err := manager.Exp.assemble(); err != nil {
-		return nil, err
+	if err := GGameConfig.Scene.assemble(); err != nil {
+		return err
+	}
+	if err := GGameConfig.Exp.assemble(); err != nil {
+		return err
 	}
 
-	return manager, nil
+	return nil
 }
